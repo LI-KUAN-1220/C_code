219e128a97786d064956bac4a07554be680f87f7
LIN MU <1601436462@qq.com>
2023-06-28 16:32:32 +0800

    初识C语言

---
+++ test_6_12/test_6_12/test.c
@@ -0,0 +1,30 @@
+////写一个c语言代码，在屏幕上打印hello world
+////
+//
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+
+
+
+
+//代码一般是从main函数开始写
+
+#include<stdio.h>
+
+//C语言中，main函数是程序的入口
+//程序是从main函数的第一行开始执行的
+
+int main()
+{
+    printf("hehe\n");
+    printf("hehe\n");
+    printf("hehe\n");
+    printf("hehe\n");
+    printf("hehe\n");
+    return 0;
+}
